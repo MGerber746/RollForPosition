f4ec03fb094cbc3375e80b0c03539d20c5df3548
Matt Gerber <mgg0516@westminstercollege.edu>
2021-01-17 19:18:35 -0700

    prototype that is badly coded

---
+++ rollerbot/main.py
@@ -0,0 +1,53 @@
+import asyncio
+import position_roller as p
+import discord
+
+client = discord.Client()
+
+class Job():
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        if Job.__instance == None:
+            Job()
+        return Job.__instance
+
+    def __init__(self):
+        if Job.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            Job.__instance = self
+            self.running = False
+            self.people = []
+
+    async def run_job(self):
+        self.running = True
+        await asyncio.sleep(10)
+        self.running = False
+        results = p.Roller(self.people).roll()
+        self.people = []
+        return results
+
+def format_message(results):
+    message = ""
+    for entry in results:
+        message += entry + ": " + results[entry].name + " "
+    return message
+
+
+@client.event
+async def on_message(message):
+
+    if message.content == '/rfp':
+        if Job.getInstance().running == True:
+            Job.getInstance().people.append(message.author.name)
+        else:
+            await message.channel.send('Roll For Position Begun. Enter /rfp in the next ten seconds to roll')
+            Job.getInstance().people.append(message.author.name)
+            result = await Job.getInstance().run_job()
+            await message.channel.send(format_message(result))
+
+
+
+client.run('ODAwNTE1NTQ1MzA4NTk0MjE2.YATQMg.cuKcAGwhbpDzZhexniFDL10QwOc')
